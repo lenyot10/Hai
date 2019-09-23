c46a65e2f4a00760a8d03693ac646757cb56b01e
Aljabar Azis <aljabarazis14@gmail.com>
2019-09-23 14:20:31 +0700

    hai

---
+++ Hai.java
@@ -0,0 +1,14 @@
+public class Hai {
+    public static void main(String[] args){
+        String kata1 = "Hai";
+        String kata2 = "Siapa lu";
+        String kata3 = "Lah gua siapa";
+        var x = "2" ;
+        var q = "4" ;
+        var z = "x*q";
+        System.out.println(z);
+        System.out.println(kata1);
+        System.out.println(kata2);
+        System.out.println(kata3);
+        }
+    }
